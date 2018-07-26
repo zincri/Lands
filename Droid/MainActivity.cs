79c0606eaa33d16d3301a4b666353bb31bef735e
Zincri <zincri_mlz@hotmail.com>
2018-07-26 17:25:05 -0500

    Flags

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
 
 namespace Lands.Droid
 {
@@ -21,7 +22,7 @@ namespace Lands.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-
+            CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
     }
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using FFImageLoading.Forms.Platform;
+//using FFImageLoading.Forms.Touch;
 using Foundation;
 using UIKit;
 
@@ -13,9 +14,8 @@ namespace Lands.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-
+            CachedImageRenderer.Init();
             LoadApplication(new App());
-
             return base.FinishedLaunching(app, options);
         }
     }
