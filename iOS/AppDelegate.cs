614971cd00c25247a8ef7fb256d4bbc1fc10eb9d
Zincri <zincri_mlz@hotmail.com>
2018-07-27 17:04:13 -0500

    jesus no sirve

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using FFImageLoading.Forms.Platform;
-//using FFImageLoading.Forms.Touch;
-using Foundation;
-using UIKit;
-
-namespace Lands.iOS
+﻿namespace Lands.iOS
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FFImageLoading.Forms.Platform;
+    //using FFImageLoading.Forms.Touch;
+    using FFImageLoading.Svg.Forms;
+    using Foundation;
+    using UIKit;
+
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            
             global::Xamarin.Forms.Forms.Init();
             CachedImageRenderer.Init();
+            var ignore = typeof(SvgCachedImage);
             LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
